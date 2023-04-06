a50fdd15ce8c4e2bcc99c73acf5e94243c708025
MohammadGhandour <mohammadghandour02@outlook.com>
2023-04-06 23:48:49 +0300

    Handled salesperson_id

--- src/functions/getSalesPerson.js
+++ src/functions/getSalesPerson.js
@@ -1,10 +1,15 @@
 const users = [
     { id: 1, name: "Mohammad" },
     { id: 2, name: "Alaa" },
+    { id: 4, name: "Mira Faraj" },
     { id: 6, name: "Fatima Diab" },
 ];
 
 
 export function getSalesPerson(id) {
-    return users.find(user => user.id === id).name;
+    if (id) {
+        return users.find(user => user.id === id).name || "";
+    } else {
+        return "";
+    }
 };
